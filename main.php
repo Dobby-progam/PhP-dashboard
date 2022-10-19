efd7f228176a5b695c9bc1b88d60190831ce00e9
Dobby-progam <dthweathertime@gmail.com>
2022-10-19 13:03:19 +0200

    update

---
+++ main.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+
+<?php include 'assets\loading\head.html'; ?>
+<?php include 'assets\loading\api-thing\cloudflaire.php'; ?>
+
+<body>
+<?php include 'assets\loading\head.html'; ?>    
+<p>hello </p>
+</body>
